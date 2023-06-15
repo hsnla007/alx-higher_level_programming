d8caff31de6956626eb2f4310d6c0024702e6c11
hsnla007 <hassanlacjggar98@gmail.com>
2023-06-15 11:02:30 -0700

    2nd try

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
+
+def find_var():
+    print(a)
+
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
+    find_var()
 
-    print(a)
